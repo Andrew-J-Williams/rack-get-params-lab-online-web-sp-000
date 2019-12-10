9c4f30469bfb1a65ece944648c60cf250cb24dca
Andrew Williams <1williamsaj@gmail.com>
2019-12-10 04:46:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,7 +24,7 @@ class Application
       end
 
     elsif req.path.match(/add/)
-      search_term = req.params["q"]
+      search_term = req.params["item"]
 
       if @@items.include?(search_term)
         resp.write "added #{search_term}"
